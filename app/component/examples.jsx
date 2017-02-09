0c71b969d5b3b885890e6b6808cbda394bc6854f
Pederson <brian.pederson@bsci.com>
2017-02-09 11:28:00 -0600

    simple update

--- app/component/examples.jsx
+++ app/component/examples.jsx
@@ -2,7 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
     return (
+      <div>
       <h3>Examples Componen!</h3>
+      <p>Welcome to examples </p>
+      </div>
     )
   };
 
